cc011dce519b7674eb1b0f036ce73b2a22156db4
League Student <league@iMac-12.attlocal.net>
2019-12-08 14:26:38 -0800

    :D

--- src/_05_typing_tutor/TypingTutor.java
+++ src/_05_typing_tutor/TypingTutor.java
@@ -1,25 +1,59 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class TypingTutor {
+public class TypingTutor implements KeyListener {
 int type;
-	
-public static void main(String[] args) {
-JFrame frame = new JFrame("Type or Die");
-frame.setVisible(true);
-frame.addKeyListener(null);
 char currentLetter;
-
-currentLetter = generateRandomLetter();
+JLabel label = new JLabel();
+JPanel panel = new JPanel();
+void setup() {
+	JFrame frame = new JFrame("Type or Die");
+	frame.setVisible(true);
+	frame.addKeyListener(this);
+	panel.add(label);
+	frame.add(panel);
+	currentLetter = generateRandomLetter();
+	label.setText(currentLetter + "");
+	label.setFont(label.getFont().deriveFont(28));
+	label.setHorizontalAlignment(label.CENTER);
+}
+public static void main(String[] args) {
+TypingTutor T = new TypingTutor();
+T.setup();
+}
 
 char generateRandomLetter() {
       Random r = new Random();
       return (char) (r.nextInt(26) + 'a');
-      
-JLabel label = new JLabel();
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("you typed " + currentLetter);
+	if (e.getKeyChar() == currentLetter) {
+	panel.setBackground(Color.GREEN);
+	}
+	else {
+	panel.setBackground(Color.red);	
+	}
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	currentLetter = generateRandomLetter();
+	label.setText(currentLetter + "");
 }
 }
 
