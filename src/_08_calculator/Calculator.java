49ccccac6d8ec8e6dd34a112febc7012e8eb0772
League Student <league@iMac-24.attlocal.net>
2019-12-22 14:27:40 -0800

    :D

--- src/_08_calculator/Calculator.java
+++ src/_08_calculator/Calculator.java
@@ -1,13 +1,16 @@
 package _08_calculator;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.temporal.JulianFields;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator {
+public class Calculator implements ActionListener {
 	
 int add(int number1, int number2){
 int sum = number1 + number2;
@@ -20,8 +23,8 @@ return difference;
 }
 
 int mult(int number1, int number2){
-int answer = number1 * number2;
-return answer;
+int product = number1 * number2;
+return product;
 }
 
 int div(int number1, int number2){
@@ -29,12 +32,69 @@ int quotient = number1/number2;
 return quotient;
 }
 
+
 JFrame frame = new JFrame("Simple Caculator");
 JPanel panel = new JPanel();
 JButton add = new JButton("add");
-JButton sub = new JButton("sub");
-JButton mult = new JButton("mult");
-JButton div = new JButton("div");
-JTextField field1 = new JTextField();
-JTextField field2 = new JTextField();
+JButton sub = new JButton("subtract");
+JButton mult = new JButton("multiply");
+JButton div = new JButton("divide");
+JTextField field1 = new JTextField(10);
+JTextField field2 = new JTextField(10);
+JLabel label = new JLabel();
+
+public static void main(String[] args) {
+Calculator calculator = new Calculator();
+calculator.setup();
+}
+
+void setup(){
+panel.add(add);
+panel.add(sub);
+panel.add(mult);
+panel.add(div);
+panel.add(field1);
+panel.add(field2);
+panel.add(label);
+frame.add(panel);
+frame.pack();
+frame.setVisible(true);
+add.addActionListener(this);
+sub.addActionListener(this);
+mult.addActionListener(this);
+div.addActionListener(this);
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	String Tf1 = field1.getText();
+	String Tf2 = field2.getText();
+	int T1 = Integer.parseInt(Tf1);
+	int T2 = Integer.parseInt(Tf2);
+	
+	 if (e.getSource() == add) {
+		int sum = add(T1, T2);
+		label.setText("" + sum);
+	}
+	 if (e.getSource() == sub) {
+			sub(T1, T2);
+				int difference = sub(T1, T2);
+				label.setText("" + difference);
+	 }
+	 
+	 if (e.getSource() == mult) {
+			mult(T1, T2);
+				int product = mult(T1, T2);
+				label.setText("" + product);
+	 }
+	 
+	 if (e.getSource() == div) {
+			div(T1, T2);
+			int quotient = div(T1, T2);
+			label.setText("" + quotient);
+		}
+
+	 
+}
 }
--- src/_08_calculator/CalculatorRunner.java
+++ src/_08_calculator/CalculatorRunner.java
@@ -1,5 +1,7 @@
 package _08_calculator;
 
 public class CalculatorRunner {
-
+public static void main(String[] args) {
+	
+}
 }
