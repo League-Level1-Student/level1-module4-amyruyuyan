8a3161ba2a1584d180c35110f50b247ed66cb1fe
League Student <league@iMac-22.attlocal.net>
2020-03-01 14:25:59 -0800

    :D

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,4 +1,18 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class PracticeTest {
+
+void setup() {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JLabel label = new JLabel();
+JTextField field = new JTextField();
+JButton button1 = new JButton("submit");
+JButton button2 = new JButton("hint");
+}
 
 }
